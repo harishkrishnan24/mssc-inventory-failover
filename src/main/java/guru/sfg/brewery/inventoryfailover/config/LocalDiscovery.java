32fddad35bfc9c29066e22f296758a92f4954632
Harish krishnan <harishkrishnan1993@gmail.com>
2020-12-07 20:21:46 +0530

    Add Router config

---
+++ src/main/java/guru/sfg/brewery/inventoryfailover/config/LocalDiscovery.java
@@ -0,0 +1,11 @@
+package guru.sfg.brewery.inventoryfailover.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableDiscoveryClient
+@Profile("local-discovery")
+public class LocalDiscovery {
+}
